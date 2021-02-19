32b8640c593a6676147e684ebfa6c22f97f5d6f0
zack dove <zdove@live.co.uk>
2021-02-19 15:43:03 +0000

    Working on concentric circles

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ const cols = 20;
 const margin = 100;
 const rows = Math.round((window.innerWidth-2*margin)*cols/(window.innerHeight-2*margin));
 var dots = [];
-
+const numCircles = 8;
 
 class Dot{
 	constructor(){
@@ -16,15 +16,32 @@ class Dot{
 		this.baseY = 0;
 		this.x = 0;
 		this.y = 0;
+		// Column / Layer in circle
 		this.xi = 0;
+		// Row / Number in layer
 		this.yi = 0;
 		this.radius = 3;
+		this.tweenStartX = 0;
+		this.tweenStartY = 0;
+		this.tweening = false;
 	}
 	
 	drawDot(){
 		noStroke();
 		fill('rgba(0,0,0,1)');
-		circle(this.x,this.y,this.radius);
+		circle(this.x,this.y,3);
+	}
+	
+	drawBaseDot(){
+		noStroke();
+		fill('rgba(0,0,0,1)');
+		circle(this.baseX,this.baseY,3);
+	}
+	
+	drawCircle(){
+		noStroke();
+		fill('rgba(0,0,0,1)');
+		circle(this.baseX,this.baseY,this.radius);
 	}
 	
 	drawLine(){
@@ -34,8 +51,10 @@ class Dot{
 	}
 	
 	draw(){
-		this.drawDot();
+		this.drawBaseDot();
+		// this.drawDot();
 		// this.drawLine();
+		// this.drawCircle();
 	}
 	
 	mouseVersion(){
@@ -53,7 +72,8 @@ class Dot{
 			var angle = Math.atan2(dy, dx);
 			this.x = this.baseX + amount * Math.cos(angle);
 			this.y = this.baseY + amount * Math.sin(angle);
-			// console.log(this.x);
+			this.radius = 0.5+ amount/5;
+			// console.log(this.radius);
 			
 		} else {
 			this.x = this.baseX;
@@ -61,17 +81,59 @@ class Dot{
 		}
 	}
 	
-	sinVersion(){
+	sinUpdate(){
 		// console.log(millis());
 		let t = millis()*0.0005;
 		// can just replace with offset rather than baseX/baseY twice
-		this.x = this.baseX + sin((t*(this.xi+0.0001)*2) + this.yi)*30;
-		this.y = this.baseY + cos((t*(this.xi+0.0001)*2) + this.yi)*30;
+		this.x = this.baseX + sin((t*(this.xi+0.0001*Math.PI/4)*2) + (this.yi*Math.PI/2))*10;
+		this.y = this.baseY + cos((t*(this.xi+0.0001*Math.PI/4)*2) + (this.yi*Math.PI/2))*10;
+		this.radius = 5+ sin((t*(this.xi+0.0001*Math.PI/4)*2) + (this.yi*Math.PI/2))*10;
+	}
+	
+	rotate(){
+		var outerR = (height/2) - margin;
+		var r = outerR * (this.xi-1)/numCircles;
+		var distBetween = 30;
+		var numInLayer = Math.round((2*r*Math.PI)/distBetween);
+		// could move all this to a class attribute, since angle wont change.
+		var angle = (2*Math.PI)/(numInLayer);
+		let t = millis()*0.0005;
+		// Rotate with varying speeds:
+		// this.baseX = centerX + (r*cos( angle*(this.yi )+  (0.5*this.xi*sin(t*0.5))));
+		// this.baseY = centerY + (r*sin( angle*(this.yi ) + (0.5*this.xi*sin(t*0.5))));
+		// Rotate with varying speeds, inverse:
+		this.baseX = centerX + (r*cos( angle*(this.yi )+  (0.5*(numCircles+1-this.xi)*sin(t*0.5))));
+		this.baseY = centerY + (r*sin( angle*(this.yi ) + (0.5*(numCircles+1-this.xi)*sin(t*0.5))));
+		// Rotate equally with time:
+		// this.baseX = centerX + (r*cos( angle*(this.yi )+  t));
+		// this.baseY = centerY + (r*sin( angle*(this.yi ) + t));
+	}
+	
+	setTweenTarget(targetX, targetY){
+		
+		this.tweenStartX = this.baseX;
+		this.tweenStartY = this.baseY;
+		this.targetX = targetX;
+		this.targetY = targetY;
+		this.tweening = true;
+	}
+	
+	tweenToTarget(){
+		this.baseX = this.baseX + (this.targetX-this.tweenStartX)/10;
+		this.baseY = this.baseY + (this.targetY-this.tweenStartY)/10;
+		if (dist(this.baseX, this.baseY, this.targetX, this.targetY)<2){
+			this.tweening = false;
+		}
 	}
 	
 	update(){
-		// this.mouseVersion();
-		this.sinVersion();
+		this.mouseVersion();
+		// this.sinUpdate();
+		if (this.tweening){
+			this.tweenToTarget();
+		} else {
+			this.rotate();
+		}
 	}
 }
 
@@ -81,15 +143,15 @@ function setupGrid(){
 			var dot = new Dot;
 			dot.baseX = ((i/(rows-1)) * (width-200))    + 100;
 			dot.baseY = ((j/(cols-1)) * (height-200))   + 100;
-			dot.xi = i;
-			dot.yi = j;
+			dot.xi = i+1;
+			dot.yi = j+1;
 			dots.push(dot);
 		}
 	}
 }
 
 function setupCircle(){
-	var numCircles = 8;
+	
 	var radii = [];
 	var outerR = (height/2) - margin;
 	var distBetween = 30;
@@ -102,12 +164,55 @@ function setupCircle(){
 			var dot = new Dot;
 			dot.baseX = centerX + (r*cos(angle*j));
 			dot.baseY = centerY + (r*sin(angle*j));
+			dot.xi = i+1;
+			dot.yi = j+1;
+			dots.push(dot);
+		}
+	}
+	console.log(dots);
+}
+
+// See solution at link below
+// https://math.stackexchange.com/questions/1681461/fit-2600-equally-spaced-points-on-concentric-circles
+function setupCircle2(){
+	var numDots = cols * rows;
+	var layers = (Math.sqrt(Math.PI) * Math.sqrt(4*numDots + Math.PI) - Math.PI)/(2*Math.PI);
+	layers = Math.round(layers);
+	var approxNumDots= 0;
+	for (var i = 1; i<=layers; i++){
+		approxNumDots += Math.floor(2*i *Math.PI);
+	}
+	var remainingDots = numDots - approxNumDots;
+	if (remainingDots > 0){
+		console.log("remaiing : "+remainingDots);
+	}
+	var newRemaining = remainingDots;
+	for (var i = 1; i<=layers; i++){
+		var baseNumInLayer = Math.floor(2*i *Math.PI);
+		var extraInLayer =  Math.floor(remainingDots*(baseNumInLayer)/numDots);
+		if (i == layers){
+			extraInLayer = newRemaining;
+		} else {
+			newRemaining -= extraInLayer;
+		}
+		var numInLayer = baseNumInLayer + extraInLayer;
+		var angle = 2*Math.PI/numInLayer;
+		for (var j = 0; j<numInLayer; j++){
+			var dot = new Dot;
+			dot.baseX = centerX + (r*cos(angle*j));
+			dot.baseY = centerY + (r*sin(angle*j));
+			dot.angle = angle*j;
 			dot.xi = i;
 			dot.yi = j;
 			dots.push(dot);
 		}
 	}
-	console.log(dots);
+}
+
+function moveAllToCenter(){
+	for (dot of dots){
+		dot.setTweenTarget(centerX, centerY);
+	}
 }
 
 function setup(){
@@ -116,12 +221,19 @@ function setup(){
 	createCanvas(width, height);
 	// setupGrid();
 	setupCircle();
+	setupCircle2();
 }
 
 function draw(){
 	clear();
+	let t = millis()/1000;
+	if (t > 5){
+		moveAllToCenter();
+	}
 	for (dot of dots){
 		dot.update();
 		dot.draw();
 	}
+	
+	
 }
