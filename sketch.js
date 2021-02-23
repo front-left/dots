49c953a3a209965fde480ea34b7fe4c50164dfd3
zack dove <zdove@live.co.uk>
2021-02-23 10:34:10 +0000

    Added delay

--- sketch.js
+++ sketch.js
@@ -11,6 +11,9 @@ var dots = [];
 const numCircles = 8;
 // 0 = none, 1 = circle, 2 = grid
 var mode = 0;
+var rotateTime = 0;
+var layers;
+const timer = ms => new Promise(res => setTimeout(res, ms));
 
 class Dot{
 	constructor(){
@@ -22,7 +25,7 @@ class Dot{
 		this.xi = 0;
 		// Row / Number in layer
 		this.yi = 0;
-		this.radius = 3;
+		this.radius = 7;
 		this.tweenStartX = 0;
 		this.tweenStartY = 0;
 		this.tweening = false;
@@ -58,7 +61,7 @@ class Dot{
 		// this.drawBaseDot();
 		// this.drawDot();
 		// this.drawLine();
-		this.drawCircle()
+		this.drawCircle();
 	}
 	
 	mouseVersion(){
@@ -98,13 +101,22 @@ class Dot{
 			// this.radius = 10+ cos(2*t)*8;
 			
 			// By layer
-			this.radius = 10 + cos(2*(t + this.xi))*8;
+			// this.radius = 10 + cos((5*t + this.xi))*8;
 			
-			// By pos in layer, double sided
-			// this.radius = 10 + cos(2*(t + this.baseAngle))*8;
+			// By layer, reverse
+			// this.radius = 10 + cos((-5*t + this.xi))*8;
+			
+			// By pos in layer, three sided
+			// this.radius = 10 + cos((5*t + 3*this.baseAngle))*8;
 			
 			// By pos in layer, single
-			// this.radius = 10 + cos(2*(t + this.baseAngle/2))*8;
+			// this.radius = 10 + cos((5*t + this.baseAngle))*8;
+			
+			// By layer and pos  = spiral
+			// this.radius = 10 + cos((7*t + this.baseAngle + this.xi))*8;
+			
+			// By layer and pos  = spiral. Reversed
+			// this.radius = 10 + cos((-7*t + this.baseAngle + this.xi))*8;
 		} else {
 			this.x = this.baseX + sin((t*(this.xi+0.0001*Math.PI/4)*2) + (this.yi*Math.PI/2))*10;
 			this.y = this.baseY + cos((t*(this.xi+0.0001*Math.PI/4)*2) + (this.yi*Math.PI/2))*10;
@@ -113,16 +125,26 @@ class Dot{
 	}
 	
 	rotate(){
-		let t = millis()*0.0005;
-		// Rotate with varying speeds:
-		// this.baseX = centerX + (r*cos( angle*(this.yi )+  (0.5*this.xi*sin(t*0.5))));
-		// this.baseY = centerY + (r*sin( angle*(this.yi ) + (0.5*this.xi*sin(t*0.5))));
-		// Rotate with varying speeds, inverse:
-		// this.baseX = centerX + (this.yi*10*cos( this.angle*(this.yi )+  (0.5*(numCircles+1-this.xi)*sin(t*0.5))));
-		// this.baseY = centerY + (this.yi*10*sin( this.angle*(this.yi ) + (0.5*(numCircles+1-this.xi)*sin(t*0.5))));
-		// Rotate equally with time:
-		this.baseX = centerX + (this.xi*30*cos( this.baseAngle+  t));
-		this.baseY = centerY + (this.xi*30*sin( this.baseAngle + t));
+		let t = (millis() - rotateTime)*0.0005;
+		// all at equal speed
+		// var newCoords = rotateCoords(centerX, centerY, this.baseX, this.baseY, 0.5);
+		// this.baseX = newCoords[0];
+		// this.baseY = newCoords[1];
+		
+		// all at speed, based on layer
+		// var newCoords = rotateCoords(centerX, centerY, this.baseX, this.baseY, 0.1*(layers-this.xi+1));
+		// this.baseX = newCoords[0];
+		// this.baseY = newCoords[1];
+		
+		// all at speed, based on layer
+		// var newCoords = rotateCoords(centerX, centerY, this.baseX, this.baseY, 0.1*(layers-this.xi+1));
+		// this.baseX = newCoords[0];
+		// this.baseY = newCoords[1];
+		
+		// all at speed, based on layer, sinusoidal
+		var newCoords = rotateCoords(centerX, centerY, this.baseX, this.baseY, sin(t+(layers-this.xi)*0.05)*0.1*(layers-this.xi+1));
+		this.baseX = newCoords[0];
+		this.baseY = newCoords[1];
 	}
 	
 	setTweenTarget(targetX, targetY){
@@ -141,106 +163,41 @@ class Dot{
 			this.tweening = false;
 			this.baseX = this.targetX;
 			this.baseY = this.targetY;
+			rotateTime = millis();
 		}
 	}
 	
 	update(){
 		// this.mouseVersion();
-		this.sinUpdate();
+		// this.sinUpdate();
 		if (this.tweening){
 			this.tweenToTarget();
 		} else {
-			// this.rotate();
+			if (mode == 1){
+				this.rotate();
+			}
 		}
+		
 		
 	}
 }
 
-function setupGrid(){
-	for (var i= 0; i<rows; i++){
-		for (var j = 0; j<cols; j++){
-			var dot = new Dot;
-			dot.baseX = ((i/(rows-1)) * (width-200))    + 100;
-			dot.baseY = ((j/(cols-1)) * (height-200))   + 100;
-			dot.xi = i+1;
-			dot.yi = j+1;
-			dots.push(dot);
-		}
-	}
-}
-
-function setupCircle(){
-	
-	var radii = [];
-	var outerR = (height/2) - margin;
-	var distBetween = 30;
-	for (var i = 1; i<numCircles; i++){
-		var r = outerR * i/numCircles;
-		var numInLayer = Math.round((2*r*Math.PI)/distBetween);
-		console.log(numInLayer);
-		var angle = (2*Math.PI)/numInLayer;
-		for (var j = 0; j<numInLayer; j++){
-			var dot = new Dot;
-			dot.baseX = centerX + (r*cos(angle*j));
-			dot.baseY = centerY + (r*sin(angle*j));
-			dot.xi = i+1;
-			dot.yi = j+1;
-			dots.push(dot);
-		}
-	}
-	console.log(dots);
+function rotateCoords(cx, cy, x, y, angle) {
+    var radians = (Math.PI / 180) * angle,
+        cos = Math.cos(radians),
+        sin = Math.sin(radians),
+        nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
+        ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
+    return [nx, ny];
 }
 
 // See solution at link below
 // https://math.stackexchange.com/questions/1681461/fit-2600-equally-spaced-points-on-concentric-circles
-function setupCircle2(){
+async function moveToCircle(){
+	var rowDelay = 0;
+	var dotDelay = 50;
 	var numDots = cols * rows;
-	console.log("numdots: "+numDots);
-	var layers = (Math.sqrt(Math.PI) * Math.sqrt(4*numDots + Math.PI) - Math.PI)/(2*Math.PI);
-	layers = Math.round(layers);
-	// Dots gained by following floor of 2.k.pi, plus add one for dot in center
-	var approxNumDots= 1;
-	for (var i = 1; i<=layers; i++){
-		approxNumDots += Math.floor(2*i *Math.PI);
-	}
-	var remainingDots = numDots - approxNumDots;
-	if (remainingDots > 0){
-		console.log("remaiing : "+remainingDots);
-	}
-	var dot = new Dot;
-	dot.baseX = centerX;
-	dot.baseY = centerY;
-	dot.xi = 0;
-	dot.yi = 0;
-	dots.push(dot);
-	var newRemaining = remainingDots;
-	for (var i = 1; i<=layers; i++){
-		var baseNumInLayer = Math.floor(2*i *Math.PI);
-		var extraInLayer =  Math.floor(remainingDots*(baseNumInLayer)/numDots);
-		if (i == layers){
-			extraInLayer = newRemaining;
-		} else {
-			newRemaining -= extraInLayer;
-		}
-		var numInLayer = baseNumInLayer + extraInLayer;
-		var angle = 2*Math.PI/numInLayer;
-		var layerRadius = i * 30;
-		for (var j = 0; j<numInLayer; j++){
-			var dot = new Dot;
-			dot.baseX = centerX + (layerRadius*cos(angle*j));
-			dot.baseY = centerY + (layerRadius*sin(angle*j));
-			dot.baseAngle = angle*j;
-			dot.xi = i;
-			dot.yi = j;
-			dots.push(dot);
-		}
-	}
-	console.log("dots"+dots.length);
-}
-
-function moveToCircle(){
-	var numDots = cols * rows;
-	var layers = (Math.sqrt(Math.PI) * Math.sqrt(4*numDots + Math.PI) - Math.PI)/(2*Math.PI);
+	layers = (Math.sqrt(Math.PI) * Math.sqrt(4*numDots + Math.PI) - Math.PI)/(2*Math.PI);
 	layers = Math.round(layers);
 	// Dots gained by following floor of 2.k.pi, plus add one for dot in center
 	var approxNumDots= 1;
@@ -252,13 +209,13 @@ function moveToCircle(){
 		console.log("remaiing : "+remainingDots);
 	}
 	var dot = dots[0];
-	dot.baseX = centerX;
-	dot.baseY = centerY;
+	dot.setTweenTarget(centerX, centerY);
 	dot.xi = 0;
 	dot.yi = 0;
 	var newRemaining = remainingDots;
 	var dotIndex = 1;
 	for (var i = 1; i<=layers; i++){
+		await timer(rowDelay);
 		var baseNumInLayer = Math.floor(2*i *Math.PI);
 		var extraInLayer =  Math.floor(remainingDots*(baseNumInLayer)/numDots);
 		if (i == layers){
@@ -270,6 +227,7 @@ function moveToCircle(){
 		var angle = 2*Math.PI/numInLayer;
 		var layerRadius = i * 30;
 		for (var j = 0; j<numInLayer; j++){
+			await timer(dotDelay);
 			var dot = dots[dotIndex];
 			dotIndex++;
 			let targetX = centerX + (layerRadius*cos(angle*j));
@@ -300,6 +258,7 @@ function moveToGrid(){
 }
 
 function moveAllToCenter(){
+	var delay = 0;
 	for (dot of dots){
 		dot.setTweenTarget(centerX, centerY);
 	}
@@ -326,8 +285,8 @@ function setup(){
 	// setupCircle();
 	// setupCircle2();
 	setTimeout(moveToCircle, 1000);
-	setInterval(moveToGrid, 16000);
-	setTimeout(function(){setInterval(moveToCircle, 16000)}, 8000);
+	setInterval(moveToGrid, 20000);
+	setTimeout(function(){setInterval(moveToCircle, 20000)}, 10000);
 }
 
 function draw(){
